d84e8aee7fabd2addf64c943e3a21e3f4011b633
arav-dhoot <65766560+arav-dhoot@users.noreply.github.com>
2022-07-26 12:30:10 +0530

    Changes to new_plants dataloader

--- new_plants_disease_dataloader.py
+++ new_plants_disease_dataloader.py
@@ -1,11 +1,17 @@
 from class_dataset import PlantImageDataset
 from torch.utils.data import DataLoader
+import torchvision.transforms as T
 
 batch_size = 64
-main_dir = "/Users/aravdhoot/CS_Research_Project/data/New Plants Diseases Dataset"
+transforms = T.Compose([T.Resize(size=(224, 224))])
+main_dir = "/Users/aravdhoot/Plant_Disease_Classification/data/New Plants Diseases Dataset"
 
-train_dataset = PlantImageDataset(csv_file= "new_plants_disease_train.csv", root_dir="/Users/aravdhoot/CS_Research_Project/data/New Plants Diseases Dataset/New Plant Diseases Dataset(Augmented)/New Plant Diseases Dataset(Augmented)/train", main_dir=main_dir)
-valid_dataset = PlantImageDataset(csv_file= "new_plants_disease_valid.csv", root_dir="/Users/aravdhoot/CS_Research_Project/data/New Plants Diseases Dataset/New Plant Diseases Dataset(Augmented)/New Plant Diseases Dataset(Augmented)/valid", main_dir=main_dir)
+train_dataset = PlantImageDataset(csv_file= "new_plants_disease_train.csv", root_dir="/Users/aravdhoot/Plant_Disease_Classification/data/New Plants Diseases Dataset/New Plant Diseases Dataset(Augmented)/New Plant Diseases Dataset(Augmented)/train", main_dir=main_dir, transform=transforms)
+valid_dataset = PlantImageDataset(csv_file= "new_plants_disease_valid.csv", root_dir="/Users/aravdhoot/Plant_Disease_Classification/data/New Plants Diseases Dataset/New Plant Diseases Dataset(Augmented)/New Plant Diseases Dataset(Augmented)/valid", main_dir=main_dir, transform=transforms)
 
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 valid_loader = DataLoader(dataset=valid_dataset, batch_size=batch_size, shuffle=False)
+
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+print(images, labels)
