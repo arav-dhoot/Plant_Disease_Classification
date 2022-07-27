08e8ad16b8cd658b3eaea29924a93071ff289dfe
arav-dhoot <65766560+arav-dhoot@users.noreply.github.com>
2022-07-27 07:56:03 +0530

    Config support added

--- src/new_plants_disease_dataloader.py
+++ src/new_plants_disease_dataloader.py
@@ -10,8 +10,8 @@ with open("/Users/aravdhoot/Plant_Disease_Classification/config.yaml", 'r') as y
 
 main_dir = parse_yaml['main_dir']['new_plants_disease']
 
-train_dataset = PlantImageDataset(csv_file=parse_yaml['csv']['new_plants_disease']['train'], root_dir="/Users/aravdhoot/Plant_Disease_Classification/data/New Plants Diseases Dataset/New Plant Diseases Dataset(Augmented)/New Plant Diseases Dataset(Augmented)/train", main_dir=main_dir, transform=transforms)
-valid_dataset = PlantImageDataset(csv_file=parse_yaml['csv']['new_plants_disease']['valid'], root_dir="/Users/aravdhoot/Plant_Disease_Classification/data/New Plants Diseases Dataset/New Plant Diseases Dataset(Augmented)/New Plant Diseases Dataset(Augmented)/valid", main_dir=main_dir, transform=transforms)
+train_dataset = PlantImageDataset(csv_file=parse_yaml['csv']['new_plants_disease']['train'], root_dir=parse_yaml['root_dir']['new_plants_disease']['train'], main_dir=main_dir, transform=transforms)
+valid_dataset = PlantImageDataset(csv_file=parse_yaml['csv']['new_plants_disease']['valid'], root_dir=parse_yaml['root_dir']['new_plants_disease']['valid'], main_dir=main_dir, transform=transforms)
 
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 valid_loader = DataLoader(dataset=valid_dataset, batch_size=batch_size, shuffle=False)
